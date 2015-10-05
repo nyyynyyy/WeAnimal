c813bbc141de96ef57a96460ef9e4860274e9641
nyyynyyy <nyyynyyy@naver.com>
2015-10-05 12:08:01 +0900

    deletemenu

--- app/src/main/java/jsc/cactus/com/weanimal/MyApp.java
+++ app/src/main/java/jsc/cactus/com/weanimal/MyApp.java
@@ -11,13 +11,5 @@ import io.socket.client.Socket;
  * Created by nyyyn on 2015-09-23.
  */
 public class MyApp extends Application {
-   /* public static Socket mSocket;
-    {
-  try {
-        mSocket = IO.socket(Variable.SERVER_ADDRESS);
-        //OftenMethod.message(this,"CREATE SOCKET");
-        } catch (URISyntaxException e) {
-        throw new RuntimeException(e);
-        }
-        }*/
-        }
+
+}
--- app/src/main/java/jsc/cactus/com/weanimal/StartActivty.java
+++ app/src/main/java/jsc/cactus/com/weanimal/StartActivty.java
@@ -42,26 +42,4 @@ public class StartActivty extends AppCompatActivity {
 
         OftenMethod.message(this, "이용해주셔서 감사합니다.");
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_start_activty, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
